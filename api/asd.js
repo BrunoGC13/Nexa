37c8d5b890d40739ff24ad89fdec9ac15dde9205
Bruno Glotz <brunoglotz@icloud.com>
2025-08-24 18:49:58 +0200

    Initial commit

--- api/asd.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-// Beispiel: SHA-256 Hash
-const input = "Hallo Welt";
-const hash = crypto.createHash("sha256").update(input).digest("hex");
-
-console.log("SHA-256:", hash);
